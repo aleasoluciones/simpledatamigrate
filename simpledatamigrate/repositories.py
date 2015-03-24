cee0632c0bb9f3acf99dcf99ade2d2d817be93a5
regiluze <regiluze@hotmail.com>
2015-03-24 18:50:30 +0100

    postgres repository creates schemaversion table if doesnt exist

--- simpledatamigrate/repositories.py
+++ simpledatamigrate/repositories.py
@@ -15,9 +15,11 @@ class SchemaVersionRepository(object):
 
 class PostgresSchemaVersionRepository(object):
 
+
     def __init__(self, connection):
         self._connection = connection
         self._connection.autocommit = True
+        self._create_table_if_not_exists()
 
     def actual_schema(self):
         with self._connection.cursor() as cursor:
@@ -29,11 +31,23 @@ class PostgresSchemaVersionRepository(object):
 
     def set_actual_schema(self, version):
         if self.actual_schema() is None:
-            self.insert_actual_schema(version)
+            self._insert_actual_schema(version)
         else:
             with self._connection.cursor() as cursor:
                 cursor.execute("update schemaVersion set version=(%s);", (version, ))
 
-    def insert_actual_schema(self, version):
+    def _insert_actual_schema(self, version):
         with self._connection.cursor() as cursor:
             cursor.execute("insert into schemaVersion (version) values (%s);", (version, ))
+
+
+    def _create_table_if_not_exists(self):
+        with self._connection.cursor() as cursor:
+            cursor.execute("select * from information_schema.tables where table_name=%s", ('schemaVersion',))
+            if not bool(cursor.rowcount):
+                self._create_schema_version_table()
+
+    def _create_schema_version_table(self):
+        with self._connection.cursor() as cursor:
+            cursor.execute("CREATE TABLE schemaVersion (id serial primary key, version text);")
+
