df5bf50697d611da757a9e067f6524be2c2f5465
Alberto Perez <aperez@alea-soluciones.com>
2015-03-24 14:04:59 +0100

    HouseKeeping

--- specs/schema_repository_spec.py
+++ specs/schema_repository_spec.py
@@ -6,24 +6,21 @@ from doublex import *
 from simpledatamigrate import repositories as r
 
 with describe('SchemaVersion repository'):
+    with before.each:
+        self.data_schema = r.SchemaVersionRepository()
+
     with context('when query schema version'):
         with context('initial version case'):
             with it('returns none as initial schema version'):
-                data_schema = r.SchemaVersionRepository()
-
-                version = data_schema.actual_schema()
+                version = self.data_schema.actual_schema()
 
                 expect(version).to(be_none)
-        with _context('there is a version'):
-            with it('returns the current schema version'):
-                data_schema = r.SchemaVersionRepository()
-                version = data_schema.actual_schema()
-                expect(version).to(equal(42))
 
     with context('when change schema version'):
         with it('returns new state'):
-               ver = '001'
-               data_schema = r.SchemaVersionRepository()
-               data_schema.set_actual_schema(ver)
-               version = data_schema.actual_schema()
-               expect(version).to(be(ver))
+               version = '001'
+               self.data_schema.set_actual_schema(version)
+
+               actual_version = self.data_schema.actual_schema()
+
+               expect(actual_version).to(be(version))
