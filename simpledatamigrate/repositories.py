195c60b381b7779fe398920819db6c18f4e70d37
regiluze <regiluze@hotmail.com>
2015-03-24 18:27:09 +0100

    postgres schema version repository completed

--- simpledatamigrate/repositories.py
+++ simpledatamigrate/repositories.py
@@ -23,8 +23,17 @@ class PostgresSchemaVersionRepository(object):
         with self._connection.cursor() as cursor:
             cursor.execute("select version from schemaVersion")
             schema = cursor.fetchall()
-            return schema['version']
+            if len(schema) == 0:
+                return None
+            return schema[0][0]
 
     def set_actual_schema(self, version):
+        if self.actual_schema() is None:
+            self.insert_actual_schema(version)
+        else:
+            with self._connection.cursor() as cursor:
+                cursor.execute("update schemaVersion set version=(%s);", (version, ))
+
+    def insert_actual_schema(self, version):
         with self._connection.cursor() as cursor:
-            pass
+            cursor.execute("insert into schemaVersion (version) values (%s);", (version, ))
