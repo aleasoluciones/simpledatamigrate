2fdbb532e5a4a521016a26186d60327117f41dd1
regiluze <regiluze@hotmail.com>
2015-03-25 17:03:43 +0100

    bugfix with table name

--- simpledatamigrate/repositories.py
+++ simpledatamigrate/repositories.py
@@ -15,6 +15,7 @@ class SchemaVersionRepository(object):
 
 class PostgresSchemaVersionRepository(object):
 
+    TABLE_NAME = 'schemaversion'
 
     def __init__(self, connection):
         self._connection = connection
@@ -23,7 +24,7 @@ class PostgresSchemaVersionRepository(object):
 
     def actual_schema(self):
         with self._connection.cursor() as cursor:
-            cursor.execute("select version from schemaVersion")
+            cursor.execute('select version from {table_name}'.format(table_name=self.TABLE_NAME))
             schema = cursor.fetchall()
             if len(schema) == 0:
                 return None
@@ -34,20 +35,21 @@ class PostgresSchemaVersionRepository(object):
             self._insert_actual_schema(version)
         else:
             with self._connection.cursor() as cursor:
-                cursor.execute("update schemaVersion set version=(%s);", (version, ))
+                cursor.execute('update {table_name} set version=(%s);'.format(table_name=self.TABLE_NAME), (version, ))
 
     def _insert_actual_schema(self, version):
         with self._connection.cursor() as cursor:
-            cursor.execute("insert into schemaVersion (version) values (%s);", (version, ))
+            cursor.execute('insert into {table_name} (version) values (%s);'.format(table_name=self.TABLE_NAME), (version, ))
 
 
     def _create_table_if_not_exists(self):
         with self._connection.cursor() as cursor:
-            cursor.execute("select * from information_schema.tables where table_name=%s", ('schemaVersion',))
-            if not bool(cursor.rowcount):
+            cursor.execute('select exists(select * from information_schema.tables where table_name=%s)', (self.TABLE_NAME,))
+            exists = cursor.fetchone()[0]
+            if not exists:
                 self._create_schema_version_table()
 
     def _create_schema_version_table(self):
         with self._connection.cursor() as cursor:
-            cursor.execute("CREATE TABLE schemaVersion (id serial primary key, version text);")
+            cursor.execute('CREATE TABLE {table_name} (id serial primary key, version text);'.format(table_name=self.TABLE_NAME))
 
