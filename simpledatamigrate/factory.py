907e57b9c7b4654ba6f41c4d33b30d62982b4513
regiluze <regiluze@hotmail.com>
2015-03-26 13:49:47 +0100

    Renamed env variables which are needed

--- simpledatamigrate/factory.py
+++ simpledatamigrate/factory.py
@@ -6,11 +6,11 @@ from simpledatamigrate import repositories as r, collector as c, migrator as m
 
 def create_postgres_migrator():
     connection = pg.connect(
-        host=os.getenv('INVENTORY_DB_PORT_5432_TCP_ADDR', 'localhost'),
-        port=os.getenv('INVENTORY_DB_PORT_5432_TCP_PORT', '5432'),
-        database=os.getenv('INVENTORY_DB_ENV_DBNAME', 'default'),
-        user=os.getenv('INVENTORY_DB_ENV_DBUSER', 'postgres'),
-        password=os.getenv('INVENTORY_DB_ENV_POSTGRES_PASSWORD', '12345')
+        host=os.getenv('COMPONENT_DB_HOST_ADDR'),
+        port=os.getenv('COMPONENT_DB_TCP_PORT'),
+        database=os.getenv('COMPONENT_DB_NAME'),
+        user=os.getenv('COMPONENT_DB_USER'),
+        password=os.getenv('COMPONENT_DB_PASSWORD')
     )
 
     schema_version_repo = r.PostgresSchemaVersionRepository(connection)
