df77f7e862187e4909e5179f46be9d0674b59421
Alberto Perez <aperez@alea-soluciones.com>
2018-01-09 09:50:30 +0100

    Updated setup_venv.sh to use pip with --upgrade option

--- dev/setup_venv.sh
+++ dev/setup_venv.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 for package in $(ls -d */); do pushd $package; if [ -e setup.py ]; then python setup.py develop; fi; popd; done
-pip install -r requirements.txt
-pip install -r requirements-dev.txt
+pip install pip --upgrade
+pip install -r requirements.txt --upgrade
+pip install -r requirements-dev.txt --upgrade
