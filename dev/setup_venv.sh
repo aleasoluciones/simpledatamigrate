c680af797746818ce35c8a5571e8670d8baabb71
Alejandro E. Brito Monedero <abrito@alea-soluciones.com>
2021-01-28 15:13:01 +0100

    [INTERNAL] Pin pip version in dev/setup_venv.sh

--- dev/setup_venv.sh
+++ dev/setup_venv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 for package in $(ls -d */); do pushd $package; if [ -e setup.py ]; then python setup.py develop; fi; popd; done
-pip install pip --upgrade
+pip install pip==20.3.4
 pip install -r requirements.txt --upgrade
 pip install -r requirements-dev.txt --upgrade
